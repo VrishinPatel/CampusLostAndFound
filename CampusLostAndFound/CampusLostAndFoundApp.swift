ed6a4cfa316e23cdaab4e20e4981be65d30ac6b5
Vrishin Patel <vpatel34@umd.edu>
2024-09-15 12:02:14 -0400

    Initial Commit

---
+++ CampusLostAndFound/CampusLostAndFoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusLostAndFoundApp.swift
+//  CampusLostAndFound
+//
+//  Created by Vrishin Patel on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CampusLostAndFoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
